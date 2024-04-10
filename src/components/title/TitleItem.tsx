7b4f2fd48104efa8b03690a801dd60a3d67d34a8
Nikolay Pilgun <a03198814s@gmail.com>
2024-04-10 20:37:01 +0500

    Title some styling markup

--- src/components/title/TitleItem.tsx
+++ src/components/title/TitleItem.tsx
@@ -4,6 +4,19 @@ export default function TitleItem({ item }: { item: IDataTitle }) {
 	console.log(item);
 
 	return (
-		<div className="w-full px-5 py-3 flex justify-between items-center"></div>
+		<div className="w-full px-8 py-2 flex justify-between items-center gap-5">
+			<div>
+				<h1 className="text-3xl font-['Pacifico'] py-3">{item.logo}</h1>
+				<h3 className="text-base">{item.description}</h3>
+			</div>
+			<div>
+				<p className="py-1">
+					<a className=" text-lg" href={`tel:${item.telephone}`}>
+						{item.telephone}
+					</a>
+				</p>
+				<address>{item.address}</address>
+			</div>
+		</div>
 	);
 }
